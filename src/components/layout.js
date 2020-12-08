1f0d84b2e02f1f371ceabb6122f6c02946ce1576
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-08 09:12:00 +0900

    graphql start

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./layout.css"
 
 const Layout = ({ children }) => {
 
---
+++ src/posts/blog-003.md
@@ -0,0 +1,14 @@
+---
+title: タイトル3
+date: '2020-12-07'
+thumbnail: '../images/image_003.jpg'
+---
+
+Gatsby
+![Sample](../images/image_001.jpg)
+
+## topic
+
+1. list1
+2. list2
+3. list3
